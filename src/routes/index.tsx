2ca8970798224838844942f47c8c8516611fde70
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-07-15 22:42:46 -0300

    fixed tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,24 +17,23 @@ export default function Mobile() {
 animate-gradient-x`;
   };
   return (
-    <div className='flex flex-col h-[100dvh] w-[100dvw] bg-bg-400 antialiased'>
-      <nav className='h-14 w-full'>
-        <div className='px-4 flex w-full mx-auto h-full items-center justify-between'>
-          <img src='/logo.svg' className='w-[41px] h-[41px]' alt='logo' />
-          <div className='flex justify-center'>
-            <span className='block text-2xl text-accent-300 '>
-              <span className={`${scoreColor()}`}>{currentScore}</span>/
-              {maxScore}
+    <div class='flex flex-col h-[100dvh] w-[100dvw] bg-bg-400 antialiased'>
+      <nav class='h-14 w-full'>
+        <div class='px-4 flex w-full mx-auto h-full items-center justify-between'>
+          <img src='/logo.svg' class='w-[41px] h-[41px]' alt='logo' />
+          <div class='flex justify-center'>
+            <span class='block text-2xl text-accent-300 '>
+              <span class={`${scoreColor()}`}>{currentScore}</span>/{maxScore}
             </span>
           </div>
         </div>
       </nav>
       <main
-        className={`flex flex-1 flex-col w-full ${navBorderColor()} rounded-xl py-[2px]`}>
-        <div className='flex flex-1 flex-col w-full bg-bg-100 rounded-xl'>
-          <div className='flex flex-col w-full py-10 px-10  justify-center items-center'>
-            <div className='flex flex-col w-full h-full items-center justify-center rounded-xl border border-primary-600 border-b-2 border-b-accent-500 bg-bg-300 '>
-              <Image
+        class={`flex flex-1 flex-col w-full ${navBorderColor()} rounded-xl py-[2px]`}>
+        <div class='flex flex-1 flex-col w-full bg-bg-100 rounded-xl'>
+          <div class='flex flex-col w-full py-10 px-10  justify-center items-center'>
+            <div class='flex flex-col w-full h-full items-center justify-center rounded-xl border border-primary-600 border-b-2 border-b-accent-500 bg-bg-300 '>
+              <img
                 src='/resources/Bulbasaur.png'
                 width='210'
                 height='210'
@@ -42,65 +41,67 @@ animate-gradient-x`;
               />
             </div>
           </div>
-          <div className='flex flex-col w-full h-full px-10 pb-10 justify-center items-center gap-3'>
-            <div className='absolute left-10'>
+          <div class='flex flex-col w-full h-full px-10 pb-10 justify-center items-center gap-3'>
+            {/*}
+            <div class='absolute left-10'>
               <MdOutlineStar
-                className='animate-star-victory opacity-0'
+                class='animate-star-victory opacity-0'
                 color='#FFDD87'
                 size='18px'
               />
             </div>
-            <div className='absolute left-7'>
+            <div class='absolute left-7'>
               <MdOutlineStar
-                className='animate-star-victory opacity-0'
+                class='animate-star-victory opacity-0'
                 color='#FFDD87'
                 size='18px'
               />
             </div>
-            <div className='absolute left-0'>
+            <div class='absolute left-0'>
               <MdOutlineStar
-                className='animate-star-victory opacity-0'
+                class='animate-star-victory opacity-0'
                 color='#FFDD87'
                 size='18px'
               />
             </div>
-            <div className='absolute right-10'>
+            <div class='absolute right-10'>
               <MdOutlineStar
-                className='animate-star-victory opacity-0'
+                class='animate-star-victory opacity-0'
                 color='#FFDD87'
                 size='18px'
               />
             </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
+            */}
+            <div class='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
+              <span class={`sen-text text-2xl text-primary-300`}>
                 Charmander
               </span>
             </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
+            <div class='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
+              <span class={`sen-text text-2xl text-primary-300`}>
                 Charmander
               </span>
             </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
+            <div class='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
+              <span class={`sen-text text-2xl text-primary-300`}>
                 Charmander
               </span>
             </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
+            <div class='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
+              <span class={`sen-text text-2xl text-primary-300`}>
                 Charmander
               </span>
             </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
+            <div class='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
+              <span class={`sen-text text-2xl text-primary-300`}>
                 Charizard
               </span>
             </div>
           </div>
         </div>
       </main>
-      <nav className='h-14 w-full'>
-        <div className='flex w-full mx-auto h-full items-center justify-between px-8'>
+      <nav class='h-14 w-full'>
+        <div class='flex w-full mx-auto h-full items-center justify-between px-8'>
           {/*}
           <MdHome color='#4C6477' size='36px' />
           <MdVideogameAsset color='#B8DFFF' size='36px' />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
